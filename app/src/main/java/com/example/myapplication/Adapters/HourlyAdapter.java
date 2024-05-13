eccf89eb46f13296cba8d5355b5010e5c2110a2d
artem <raz_etka_2010@mail.ru>
2024-05-13 22:31:00 +0300

    pog

--- app/src/main/java/com/example/myapplication/Adapters/HourlyAdapter.java
+++ app/src/main/java/com/example/myapplication/Adapters/HourlyAdapter.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,18 +11,37 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Domains.Hourly;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
 
 public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder> {
+ArrayList<Hourly> items;
+Context context;
+
+    public HourlyAdapter(ArrayList<Hourly> items) {
+        this.items = items;
+    }
 
     @NonNull
     @Override
     public HourlyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_holder,parent,false);
+        context = parent.getContext();
+        return new ViewHolder((inflate));
     }
 
     @Override
     public void onBindViewHolder(@NonNull HourlyAdapter.ViewHolder holder, int position) {
+        holder.hourTxt.setText(items.get(position).getHour());
+        holder.tempTxt.setText(items.get(position).getTemp());
 
+        int drawableResourceId = holder.itemView.getResources()
+                .getIdentifier(items.get(position).getPicPath(),"drawable",holder.itemView.getContext().getPackageCodePath());
+
+        Glide.with(context)
+                .load(drawableResourceId)
+                .intro(holder.pic);
     }
 
     @Override
@@ -28,10 +49,13 @@ public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder
         return 0;
     }
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView hour, Temp;
+        TextView hourTxt, tempTxt;
         ImageView pic;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            hourTxt = itemView.findViewById(R.id.hourTxt);
+            tempTxt = itemView.findViewById(R.id.tempTxt);
+            pic = itemView.findViewById(R.id.pic);
         }
     }
 }
